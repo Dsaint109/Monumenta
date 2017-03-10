f338bb5a576a1f6d8ac7170d0ac3cb316494f668
Nick Saint <topstronics@gmail.com>
2017-03-10 23:45:25 +0100

    Addition of Google annalytics to the app

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -227,3 +227,13 @@ Author URL: http://saintswebnology.com
 <script type="text/javascript">
     $('.main-banner').parallax({imageSrc: 'images/banner.jpg'});
 </script>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-93504971-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
