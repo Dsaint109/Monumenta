26f361f8b64772e910685e52f4e3ccd9581504a3
Nick Saint <topstronics@gmail.com>
2017-03-11 00:37:32 +0100

    Trying to load the google fonts after page loads

--- resources/views/includes/appDownload.blade.php
+++ resources/views/includes/appDownload.blade.php
@@ -1,7 +1,7 @@
 <div class="mobile-app">
     <div class="container">
         <div class="col-md-5 app-left" data-aos="fade-right" >
-            <a href="{{ route('appdownload') }}"><img src="images/app2.png" alt=""></a>
+            <a href="{{ route('appdownload') }}"><img src="images/app2.png" alt="Monumenta.biz app"></a>
         </div>
         <script type="text/javascript">
 
@@ -11,10 +11,10 @@
             <p>Monumenta.biz, a portal crafted for you to find whatever it is you need as fast and efficient as possible. We are a web directory that helps agencies and entrepreneurs connect with the world fast and seamlessly. Your satisfaction is our major priority.</p>
             <div class="app-buttons">
                 <div class="app-button">
-                    <a href="#"><img src="images/1.png" alt=""></a>
+                    <a href="#"><img src="images/1.png" alt="Monumenta.biz on Google Play"></a>
                 </div>
                 <div class="app-button">
-                    <a href="#"><img src="images/2.png" alt=""></a>
+                    <a href="#"><img src="images/2.png" alt="Monumenta.biz on App Store"></a>
                 </div>
                 <div class="clearfix"> </div>
             </div>
