e042a2c9948b425c18dbb1058524c0ab6d7da692
Nick Saint <topstronics@gmail.com>
2017-03-11 01:42:20 +0100

    Trying to load the google fonts after page loads

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -101,12 +101,12 @@ Author URL: http://saintswebnology.com
 
 <!-- Source -->
 <script async src="{{ URL::to('js/modernizr.custom.js')  }}"></script>
-<script async src="{{ URL::to('js/jquery.uls.data.js')  }}"></script>
-<script async src="{{ URL::to('js/jquery.uls.data.utils.js')  }}"></script>
+<script src="{{ URL::to('js/jquery.uls.data.js')  }}"></script>
+<script src="{{ URL::to('js/jquery.uls.data.utils.js')  }}"></script>
 <script async src="{{ URL::to('js/jquery.uls.lcd.js')  }}"></script>
 <script async src="{{ URL::to('js/jquery.uls.languagefilter.js')  }}"></script>
 <script async src="{{ URL::to('js/jquery.uls.regionfilter.js')  }}"></script>
-<script async src="{{ URL::to('js/jquery.uls.core.js')  }}"></script>
+<script src="{{ URL::to('js/jquery.uls.core.js')  }}"></script>
 <script async src="{{ URL::to('js/jquery.leanModal.min.js') }}"></script>
 <script async src="{{ URL::to('js/jquery.validate.min.js')  }}"></script>
 <script async src="{{ URL::to('js/sweetalert.min.js')  }}"></script>
@@ -147,10 +147,10 @@ Author URL: http://saintswebnology.com
         }
     });
 </script>
-<script async src="{{ URL::to('js/aos.js') }}"></script>
-<script async src="{{ URL::to('js/parallax.min.js') }}"></script>
-<script async src="{{ URL::to('js/main.js') }}"></script>
-<script async type="text/javascript">
+<script src="{{ URL::to('js/aos.js') }}"></script>
+<script src="{{ URL::to('js/parallax.min.js') }}"></script>
+<script src="{{ URL::to('js/main.js') }}"></script>
+<script type="text/javascript">
     AOS.init({
         easing: 'ease-in-out-sine'
     });
